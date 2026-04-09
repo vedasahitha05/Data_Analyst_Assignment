4562d271fa246018e4281c5623d75c9a74e2523e
vedasahitha05 <vedasahithakambhampati@gmail.com>
2026-04-09 11:50:07 +0530

    Update 02_Remove_Duplicates.py

--- Python/02_Remove_Duplicates.py
+++ Python/02_Remove_Duplicates.py
@@ -1,8 +1,11 @@
+# Take input string from the user
 s = input("Enter string: ")
+# Initialize an empty string to store unique characters
 result = ""
-
+# Loop through each character in the input string
 for ch in s:
+    # If the character is not already in result, add it
     if ch not in result:
         result += ch
-
+# Print the string containing only unique characters
 print("Unique string:", result)
